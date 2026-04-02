a081ea38b4b96aa79652eb6e0c4a045f0f9dec0f
Igor Kireev <igorkireev@Mac.lan>
2026-04-02 19:22:29 +0300

    add db

---
+++ service/db/__init__.py
@@ -0,0 +1,5 @@
+from service.db.accessor import get_session
+from service.db.base_model import Base
+
+
+__all__ = ["get_session", "Base"]
---
+++ service/db/accessor.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+)
+
+from service.settings import settings
+
+
+async_engine = create_async_engine(
+    url=settings.database_url_asyncpg, echo=False, pool_size=5, max_overflow=10
+)
+
+async_session_factory = async_sessionmaker(
+    async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session_factory() as session:
+        yield session
