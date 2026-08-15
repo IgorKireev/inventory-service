23073db6c74f193f9c23113b9f9d1643d4d96c32
Igor Kireev <igorkireev@Mac.lan>
2026-08-15 12:06:20 +0300

    refactor

--- service/main.py
+++ service/main.py
@@ -1,11 +1,5 @@
 from faststream import FastStream
 
 from service.broker.broker import broker
-from service.broker.consumer import router as consumer_router
-from service.service.inventory import router as publisher_router
-
-broker.include_router(consumer_router)
-broker.include_router(publisher_router)
-
 
 app = FastStream(broker)
